ad6ea22c077116ab0c885c407b70b780a33cbc57
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2023-10-08 23:11:20 +0600

    navbar work done

--- src/Pages/Home/BreakingNews.jsx
+++ src/Pages/Home/BreakingNews.jsx
@@ -4,9 +4,11 @@ import { Link } from "react-router-dom";
 const BreakingNews = () => {
     return (
         <div>
-            <div className="flex justify-between items-center">
+            <div className="flex justify-between items-center bg-[#F3F3F3] p-4 rounded-lg">
                 <button className="btn bg-[#D72050] py-2 px-6 text-white">Latest</button>
                 <marquee behavior="scroll" direction="left" loop="1" > 
+                <Link to={'/'} className="mr-4">Website developed by <span className="text-red-500">Hossain Ahmmed Taufiq</span></Link>
+                <Link to={'/'} className="mr-4">Website developed by <span className="text-red-500">Hossain Ahmmed Taufiq</span></Link>
                 <Link to={'/'} className="mr-4">Website developed by <span className="text-red-500">Hossain Ahmmed Taufiq</span></Link>
                 <Link to={'/'} className="mr-4">Website developed by <span className="text-red-500">Hossain Ahmmed Taufiq</span></Link>
                 <Link to={'/'} className="mr-4">Website developed by <span className="text-red-500">Hossain Ahmmed Taufiq</span></Link>
